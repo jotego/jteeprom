81282bb91f8d9b3f3ab16f52c5a199c405444747
Jose Tejada <jotego@gmail.com>
2020-12-09 18:23:35 +0100

    Moved to dual memory architecture

--- hdl/jt9346.v
+++ hdl/jt9346.v
@@ -16,8 +16,6 @@
     Version: 1.0
     Date: 21-3-2020 */
 
-`timescale 1ns/1ps
-
 // Module compatible with Microchip 96C06/46
 
 module jt9346(
@@ -28,16 +26,18 @@ module jt9346(
     input      sdi,         // serial data in
     output reg sdo,         // serial data out and ready/not busy signal
     input      scs          // chip select, active high. Goes low in between instructions
+    // Dump access -To do-
 );
 
 parameter AW=6, DW=16;
 
-reg  [DW-1:0] mem[0:2**AW-1];
 reg           erase_en, write_all;
-reg           last_sclk;
+reg           last_sclk, mem_we;
+reg  [   1:0] dout_up;
 wire          sclk_posedge = sclk && !last_sclk;
 reg  [AW-1:0] addr, cnt;
-reg  [DW-1:0] newdata, dout;
+reg  [DW-1:0] newdata, dout, mem_din;
+wire [DW-1:0] qout;
 reg  [  15:0] rx_cnt;
 reg  [   1:0] op;
 reg  [   6:0] st;
@@ -49,6 +49,21 @@ localparam IDLE=7'd1, RX=7'd2, READ=7'd4, WRITE=7'd8, WRITE_ALL=7'h10,
            PRE_READ=7'h20, WAIT=7'h40;
 
 always @(posedge clk) last_sclk <= sclk;
+
+jt9346_dual_ram #(.DW(DW), .AW(AW)) u_ram(
+    .clk0   ( clk   ),
+    .clk1   ( clk   ),
+    // First port: internal use
+    .addr0  ( addr      ),
+    .data0  ( mem_din   ),
+    .we0    ( mem_we    ),
+    .q0     ( qout      ),
+    // Second port: dump
+    .addr1  ( addr      ),
+    .data1  (           ),
+    .we1    ( 1'b0      ),
+    .q1     (           )
+);
 
 `ifdef JT9346_SIMULATION
     `define REPORT_WRITE(a,v) $display("EEPROM: %X written to %X", v, a);
@@ -68,6 +83,157 @@ always @(posedge clk) last_sclk <= sclk;
     `define REPORT_WRITEALL
 `endif
 
+always @(posedge clk, posedge rst) begin
+    if( rst ) begin
+        erase_en <= 0;
+        cnt      <= 0;
+        newdata  <= {DW{1'b1}};
+        mem_we   <= 0;
+        st       <= IDLE;
+        sdo      <= 1'b0;
+        dout_up  <= 0;
+    end else begin
+        if( dout_up[0] ) begin
+            `REPORT_READ ( addr, qout )
+            dout    <= qout;
+            dout_up <= 0;
+        end
+        mem_we  <= 0;
+        dout_up <= dout_up>>1;
+        if( !scs ) begin
+            st <= IDLE;
+        end else begin
+            case( st )
+                default: begin
+                    sdo    <= 1; // ready
+                    if( sclk_posedge && scs && sdi ) begin
+                        st <= RX;
+                        rx_cnt <= 16'h1 << (AW+1);
+                    end
+                end
+                RX: if( sclk_posedge && scs ) begin
+                    rx_cnt <= rx_cnt>>1;
+                    { op, addr } <= { full_op[AW:0], sdi };
+                    if( rx_cnt[0] ) begin
+                        case( full_op[AW:AW-1] ) // op is in bits 6:5
+                            2'b10: begin
+                                st      <= READ;
+                                sdo     <= 0;
+                                dout_up <= 2'b10;
+                                // dout <= mem[ next_addr ];
+                                rx_cnt  <= 16'hFFFF;
+                            end
+                            2'b01: begin
+                                st        <= WRITE;
+                                rx_cnt    <= 16'h8000;
+                                write_all <= 1'b0;
+                            end
+                            2'b11: begin // ERASE
+                                `REPORT_ERASE(next_addr);
+                                mem_din <= {DW{1'b1}};
+                                mem_we  <= 1;
+                                // mem[ next_addr ] <= {DW{1'b1}};
+                                st <= WAIT;
+                            end
+                            2'b00:
+                                case( full_op[AW-2:AW-3] )
+                                    2'b11: begin
+                                        `REPORT_ERASEEN
+                                        erase_en <= 1'b1;
+                                        st <= WAIT;
+                                    end
+                                    2'b00: begin
+                                        `REPORT_ERASEDIS
+                                        erase_en <= 1'b0;
+                                        st <= WAIT;
+                                    end
+                                    2'b10: begin
+                                        if( erase_en ) begin
+                                            `REPORT_ERASEALL
+                                            sdo     <= 0; // busy
+                                            cnt     <= 0;
+                                            newdata <= {DW{1'b1}};
+                                            st      <= WRITE_ALL;
+                                        end else begin
+                                            st <= WAIT;
+                                        end
+                                    end
+                                    2'b01: begin
+                                        `REPORT_WRITEALL
+                                        sdo       <= 0; // busy
+                                        st        <= WRITE;
+                                        newdata   <= {DW{1'b1}};
+                                        rx_cnt    <= 16'h1 << (DW-1);
+                                        write_all <= 1'b1;
+                                    end
+                                endcase
+                        endcase
+                    end
+                end
+                WRITE: if( sclk_posedge && scs ) begin
+                    newdata <= next_data;
+                    rx_cnt  <= rx_cnt>>1;
+                    sdo     <= 0; // busy
+                    if( rx_cnt[0] ) begin
+                        mem_we  <= 1;
+                        if( write_all ) begin
+                            addr   <= 0;
+                            mem_din<= next_data;
+                            st   <= WRITE_ALL;
+                        end else begin
+                            `REPORT_WRITE( addr, next_data )
+                            // mem[ addr ] <= next_data;
+                            mem_din <= next_data;
+                            st <= WAIT;
+                        end
+                    end
+                end
+                /*
+                PRE_READ: if( sclk_posedge && scs ) begin
+                    st <= READ;
+                    sdo <= 0;
+                end else if(!scs) st<=IDLE;*/
+                READ: if( sclk_posedge && scs ) begin
+                    { sdo, dout} <= { dout, 1'b1 };
+                end else if(!scs) begin
+                    st <= IDLE;
+                end
+                WRITE_ALL: begin
+                    addr   <= addr+1'd1;
+                    mem_we <= 1;
+                    if( &addr ) st <= WAIT;
+                end
+                WAIT: begin
+                    if (!scs) st<=IDLE;
+                end
+            endcase
+        end
+    end
+end
+
+endmodule
+
+
+module jt9346_dual_ram #(parameter DW=8, AW=10) (
+    input   clk0,
+    input   clk1,
+    // Port 0
+    input   [DW-1:0] data0,
+    input   [AW-1:0] addr0,
+    input   we0,
+    output reg [DW-1:0] q0,
+    // Port 1
+    input   [DW-1:0] data1,
+    input   [AW-1:0] addr1,
+    input   we1,
+    output reg [DW-1:0] q1
+    `ifdef JTFRAME_DUAL_RAM_DUMP
+    ,input dump
+    `endif
+);
+
+(* ramstyle = "no_rw_check" *) reg [DW-1:0] mem[0:(2**AW)-1];
+
 `ifdef SIMULATION
 integer rstcnt;
 initial begin
@@ -76,113 +242,14 @@ initial begin
 end
 `endif
 
-always @(posedge clk, posedge rst) begin
-    if( rst ) begin
-        erase_en <= 0;
-        cnt      <= 0;
-        newdata  <= {DW{1'b1}};
-        st       <= IDLE;
-        sdo      <= 1'b0;
-    end else if( !scs ) st <= IDLE;
-    else begin
-        case( st )
-            default: begin
-                sdo <= 1; // ready
-                if( sclk_posedge && scs && sdi ) begin
-                    st <= RX;
-                    rx_cnt <= 16'h1 << (AW+1);
-                end
-            end
-            RX: if( sclk_posedge && scs ) begin
-                rx_cnt <= rx_cnt>>1;
-                { op, addr } <= { full_op[AW:0], sdi };
-                if( rx_cnt[0] ) begin
-                    case( full_op[AW:AW-1] ) // op is in bits 6:5
-                        2'b10: begin
-                            st     <= READ;
-                            sdo    <= 0;
-                            `REPORT_READ ( next_addr, mem[ next_addr ] )
-                            dout   <= mem[ next_addr ];
-                            rx_cnt <= 16'hFFFF;
-                        end
-                        2'b01: begin
-                            st        <= WRITE;
-                            rx_cnt    <= 16'h8000;
-                            write_all <= 1'b0;
-                        end
-                        2'b11: begin // ERASE
-                            `REPORT_ERASE(next_addr);
-                            mem[ next_addr ] <= {DW{1'b1}};
-                            st <= WAIT;
-                        end
-                        2'b00:
-                            case( full_op[AW-2:AW-3] )
-                                2'b11: begin
-                                    `REPORT_ERASEEN
-                                    erase_en <= 1'b1;
-                                    st <= WAIT;
-                                end
-                                2'b00: begin
-                                    `REPORT_ERASEDIS
-                                    erase_en <= 1'b0;
-                                    st <= WAIT;
-                                end
-                                2'b10: begin
-                                    if( erase_en ) begin
-                                        `REPORT_ERASEALL
-                                        sdo     <= 0; // busy
-                                        cnt     <= 0;
-                                        newdata <= {DW{1'b1}};
-                                        st      <= WRITE_ALL;
-                                    end else begin
-                                        st <= WAIT;
-                                    end
-                                end
-                                2'b01: begin
-                                    `REPORT_WRITEALL
-                                    sdo       <= 0; // busy
-                                    st        <= WRITE;
-                                    newdata   <= {DW{1'b1}};
-                                    rx_cnt    <= 16'h1 << (DW-1);
-                                    write_all <= 1'b1;
-                                end
-                            endcase
-                    endcase
-                end
-            end
-            WRITE: if( sclk_posedge && scs ) begin
-                newdata <= next_data;
-                rx_cnt  <= rx_cnt>>1;
-                sdo     <= 0; // busy
-                if( rx_cnt[0] ) begin
-                    if( write_all ) begin
-                        cnt <= 0;
-                        st  <= WRITE_ALL;
-                    end else begin
-                        `REPORT_WRITE( addr, next_data )
-                        mem[ addr ] <= next_data;
-                        st <= WAIT;
-                    end
-                end
-            end
-            /*
-            PRE_READ: if( sclk_posedge && scs ) begin
-                st <= READ;
-                sdo <= 0;
-            end else if(!scs) st<=IDLE;*/
-            READ: if( sclk_posedge && scs ) begin
-                { sdo, dout} <= { dout, 1'b1 };
-            end else if(!scs) begin
-                st <= IDLE;
-            end
-            WRITE_ALL: begin
-                mem[cnt] <= newdata;
-                cnt <= cnt+6'd1;
-                if( &cnt ) st <= WAIT;
-            end
-            WAIT: if (!scs) st<=IDLE;
-        endcase
-    end
+always @(posedge clk0) begin
+    q0 <= mem[addr0];
+    if(we0) mem[addr0] <= data0;
+end
+
+always @(posedge clk1) begin
+    q1 <= mem[addr1];
+    if(we1) mem[addr1] <= data1;
 end
 
 endmodule
